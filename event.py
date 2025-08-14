986ba8cac72a3d89e116305ca74567269c97bb62
Coco Zeng <64930215+superasymmetry@users.noreply.github.com>
2025-08-13 22:41:02 -0400

    tracker worksssssss!!!!!!!

--- event.py
+++ event.py
@@ -1,123 +1,5 @@
-# import sys
-# import mouse as mouse
-# import keyboard
-# import time
-# import queue
-# import uiautomation as auto
-# from agents.extract import extractModel
 
-# # Queue to pass click data from callback thread to main thread
-# click_queue = queue.Queue()
-
-
-# class Event:
-#     def __init__(self):
-#         self.__listeners = []
-
-#     # Define a getter for the 'on' property which returns the decorator.
-#     @property
-#     def on(self):
-#         # A declorator to run addListener on the input function.
-#         def wrapper(func):
-#             self.addListener(func)
-#             return func
-#         return wrapper
-    
-#     def addListener(self,func):
-#         if func in self.__listeners: return
-#         self.__listeners.append(func)
-    
-#     def removeListener(self,func):
-#         if func not in self.__listeners: return
-#         self.__listeners.remove(func)
-    
-#     # Trigger events
-#     def trigger(self,args = None):
-#         # Run all the functions that are saved.
-#         if args is None:
-#             args = []
-#         for func in self.__listeners: func(*args)
-
-
-# evn = Event()  # For mouse events
-# key_evn = Event()  # For keyboard events
-# extract = extractModel()
-
-# @evn.on
-# def on_mouse_click(x, y):
-#     print(f"Click detected at ({x}, {y})")  # Immediate feedback
-#     # Just queue the coordinates for processing in main thread (much faster)
-
-# def process_ui_automation(x, y):
-#     """Process UI automation in main thread where COM is already initialized"""
-#     try:
-#         clickable_elements = extract.get_clickable_elements()
-#         element_name = None
-#         min_area = float('inf')
-#         for name, coords in clickable_elements.items():
-#             if(coords[0] < x and coords[2] > x and coords[1] < y and coords[3] > y):
-#                 if(y>1776 and y<1920):  # check if it is in the taskbar position
-#                     area = (coords[2]-coords[0])*(coords[3]-coords[1])
-#                     if(area < min_area):
-#                         min_area = area
-#                         element_name = name
-
-#         active_window_elements = extract.get_active_window_elements()
-#         for name, coords in active_window_elements.items():
-#             if(coords[0] < x and coords[2] > x and coords[1] < y and coords[3] > y):
-#                 area = (coords[2]-coords[0])*(coords[3]-coords[1])
-#                 if(area < min_area):
-#                     min_area = area
-#                     element_name = name
-#         print(f"UI Element: {element_name}")
-#     except Exception as e:
-#         print(f"Error in UI automation: {e}")
-
-# def handle_click():
-#     x, y = mouse.get_position()
-#     evn.trigger([x, y])  # Immediate click detection
-#     click_queue.put((x, y))  # Queue for UI automation
-
-# @key_evn.on
-# def on_key_press(key):
-#   print("Key pressed: ", key)
-
-#   if hasattr(key, "char") and key.char == "z":
-#     print("Z PRESSED!")
-
-# def handle_key_press(event):
-#   key = event.name
-#   key_evn.trigger([key])  # Use key_evn for keyboard events
-
-# def on_key_release(key):
-#   print("Key released: ", key)
-#   # if you need to check for a special key like shift you can
-#   # do so like this:
-#   if key == "shift":
-#     print("SHIFT KEY RELEASED!")
-
-# keyboard.on_press(on_key_press)
-# keyboard.on_release(on_key_release)
-
-# mouse.on_click(handle_click)
-
-# if __name__ == '__main__':
-#     print("tracking .. press ctrl+c to stop")
-
-#     try:
-#         while True:
-#             # Process any clicks in the queue for UI automation
-#             try:
-#                 x, y = click_queue.get_nowait()
-#                 process_ui_automation(x, y)
-#             except queue.Empty:
-#                 pass
-#             time.sleep(0.1)
-#     except KeyboardInterrupt:
-#         print("\nstopped")
-#         sys.exit(0)
-
-
+import uiautomation as auto
 import pyautogui, sys
 import mouse as mouse
 import keyboard
@@ -146,65 +28,76 @@ class Event:
         if func not in self.__listeners: return
         self.__listeners.remove(func)
     
-    # Trigger events
     def trigger(self,args = None):
-        # Run all the functions that are saved.
         if args is None:
             args = []
         for func in self.__listeners: func(*args)
 
-evn = Event()
-scroll_evn = Event()  # Fixed name
-key_evn = Event()     # Fixed name  
-extract = extractModel()
+class UIAutomationEventHandler:
+    def __init__(self):
+        self.listeners = []
+        
+    def add_listener(self, func):
+        self.listeners.append(func)
+    
+    def trigger_event(self, element_info):
+        for listener in self.listeners:
+            listener(element_info)
 
-@evn.on
-def on_mouse_click(x, y):
-    print(f"Clicked at ({x}, {y})")
+# global event handler
+click_handler = UIAutomationEventHandler()
+
+evn = Event()
+scroll_evn = Event()
+key_evn = Event()
+extract = extractModel()
+sequence = []
 
 @scroll_evn.on
 def on_mouse_scroll(x, y, delta):
-    if delta > 0:
-        direction = "up"
-    else:
-        direction = "down"
-    print(f"Scrolled at {x}, {y}. Direction is {direction}, Delta: {delta}")
+    sequence.append(f"Scrolled at {x}, {y} with delta: {delta}")
 
 @key_evn.on
 def on_key_press(key):
-    print("Key pressed: ", key)
+    sequence.append(f"Key pressed: {key}")
 
 def handle_click():
     x, y = mouse.get_position()
-    # clickable_elements = extract.get_clickable_elements()
-    # element_name = None
-    # min_area = float('inf')
-    # for name, coords in clickable_elements.items():
-    #     if(coords[0] < x and coords[2] > x and coords[1] < y and coords[3] > y):
-    #         if(y>1776 and y<1920):  # check if it is in the taskbar position
-    #             area = (coords[2]-coords[0])*(coords[3]-coords[1])
-    #             if(area < min_area):
-    #                 min_area = area
-    #                 element_name = name
+    try:
+        with auto.UIAutomationInitializerInThread():
+            element = auto.ControlFromPoint(x, y)
+            if element:
+                element_info = {
+                    'name': element.Name,
+                    'type': element.ControlTypeName,
+                    'coordinates': (x, y),
+                    'bounds': element.BoundingRectangle
+                }
+                click_handler.trigger_event(element_info)
+    except Exception as e:
+        return ""
 
-    # active_window_elements = extract.get_active_window_elements()
-    # for name, coords in active_window_elements.items():
-    #     if(coords[0] < x and coords[2] > x and coords[1] < y and coords[3] > y):
-    #         area = (coords[2]-coords[0])*(coords[3]-coords[1])
-    #         if(area < min_area):
-    #             min_area = area
-    #             element_name = name
     evn.trigger([x, y])
 
+@click_handler.add_listener
+def on_uia_clicked(element_info):
+    sequence.append(f"Clicked {element_info['name']} ({element_info['type']})")
+
+
 def handle_scroll(x, y, delta):
-    """Handle mouse scroll events"""
     scroll_evn.trigger([x, y, delta])
 
 def handle_keypress(key):
-    key_evn.trigger([key])
+    key_str = str(key)
+    if key_str.endswith(' down)'):
+        key_str = key_str[:-6]
+    # check for starts with
+    if key_str.startswith('KeyboardEvent('):
+        key_str = key_str[14:]
+    key_evn.trigger([key_str])
 
 def mouse_event_handler(event):
-    """Handle all mouse events including scroll"""
+    # handle all mouse events including scroll
     if isinstance(event, mouse.ButtonEvent):
         if event.event_type == mouse.DOWN:
             handle_click()
@@ -218,7 +111,7 @@ mouse.hook(mouse_event_handler)
 keyboard.on_press(handle_keypress)
 
 if __name__ == '__main__':
-    
+
     print("tracking .. press ctrl+c to stop")
     
     try:
@@ -226,4 +119,24 @@ if __name__ == '__main__':
             time.sleep(0.1)
     except KeyboardInterrupt:
         print("\nstopped")
+        print(sequence)
+        seq = []
+        i = 0
+        while i < len(sequence):
+            if "Key pressed:" in sequence[i]:
+                temp = ""
+                while i < len(sequence) and "Key pressed:" in sequence[i]:
+                    # parse the key pressed
+                    key = sequence[i].split(": ")[1]
+                    temp += key
+                    temp += " "
+                    i += 1
+                seq.append(f"Keys pressed: {temp.strip()}")
+                if i < len(sequence):
+                    seq.append(sequence[i])
+                    i += 1
+            else:
+                seq.append(sequence[i])
+                i += 1
+        print(seq)
         sys.exit(0)
